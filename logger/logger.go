56108901e5fd7523c195f157567b095aff70ecc2
Patrick Mahoney <patrick.mahoney@raise.com>
2018-02-07 09:32:18 -0600

    Implement logger rpc calls for thread safety.

--- logger/logger.go
+++ logger/logger.go
@@ -4,38 +4,110 @@ import (
 	"fmt"
 )
 
+type atRequest struct {
+	Index     int
+	ReplyChan chan<- Message
+}
+
+// Logger is a logging sink that stores a fixed number of log
+// messages. New messages will overwrite the oldest message; messages
+// are stored in a circular buffer.
+//
+// Caller is notified of new logs via the Updated channel, from which
+// messages must be received to avoid deadlock of loggers.
 type Logger struct {
 	// When new messages are appended to the log, a message will
 	// be written here. This channel must be read to avoid
 	// deadlock.
-	Updated  chan bool
-	input    chan Message
-	messages CircularBuffer
+	Updated     chan bool
+	input       chan<- Message
+	atRequests  chan<- atRequest
+	lenRequests chan<- chan<- int
+
+	messages []Message
+	first    int
 }
 
 func New(capacity int) *Logger {
-	return &Logger{
-		Updated:  make(chan bool),
-		input:    make(chan Message),
-		messages: NewCircularBuffer(capacity),
+	input := make(chan Message)
+	atRequests := make(chan atRequest)
+	lenRequests := make(chan chan<- int)
+
+	logger := &Logger{
+		Updated:     make(chan bool),
+		input:       input,
+		atRequests:  atRequests,
+		lenRequests: lenRequests,
+		messages:    make([]Message, 0, capacity),
+		first:       0,
 	}
+
+	go func() {
+		for {
+			select {
+			case message, ok := <-input:
+				if !ok {
+					return
+				}
+				logger.append(message)
+			case request, ok := <-atRequests:
+				if !ok {
+					continue
+				}
+				request.ReplyChan <- logger.at(request.Index)
+			case replyChan, ok := <-lenRequests:
+				if !ok {
+					continue
+				}
+				replyChan <- len(logger.messages)
+			}
+		}
+	}()
+
+	return logger
 }
 
 func (logger *Logger) Len() int {
-	return logger.messages.Len()
+	return len(logger.messages)
+
+	replyChan := make(chan int)
+	logger.lenRequests <- replyChan
+	reply, ok := <-replyChan
+	if !ok {
+		return 0
+	}
+	return reply
+
 }
 
 func (logger *Logger) At(i int) Message {
-	return logger.messages.At(i)
+	replyChan := make(chan Message)
+	request := atRequest{Index: i, ReplyChan: replyChan}
+	logger.atRequests <- request
+	reply, ok := <-replyChan
+	if !ok {
+		return Message{Message: "internal error: bad reply channel", Level: Error}
+	}
+	return reply
 }
 
-func (logger *Logger) Log(m Message) {
-	logger.messages.Append(m)
+func (logger *Logger) append(m Message) {
+	if len(logger.messages) == cap(logger.messages) {
+		logger.messages[logger.first] = m
+		logger.first = (logger.first + 1) % len(logger.messages)
+	} else {
+		logger.messages = append(logger.messages, m)
+	}
+
 	logger.Updated <- true
 }
 
+func (logger *Logger) at(i int) Message {
+	return logger.messages[logger.first+i%len(logger.messages)]
+}
+
 func (logger *Logger) Logf(level LogLevel, format string, args ...interface{}) {
-	logger.Log(Message{level, fmt.Sprintf(format, args...)})
+	logger.input <- Message{level, fmt.Sprintf(format, args...)}
 }
 
 func (logger *Logger) Infof(format string, args ...interface{}) {
---
+++ logger/logger_test.go
@@ -0,0 +1,56 @@
+package logger
+
+import (
+	"testing"
+)
+
+func TestLogger(t *testing.T) {
+	buf := New(3)
+	go func() {
+		// consume but ignore Updated events
+		for {
+			<-buf.Updated
+		}
+	}()
+
+	if buf.Len() != 0 {
+		t.Fatalf("buf.Len() should be %d but was %d", 0, buf.Len())
+	}
+	buf.Infof("zero")
+	buf.Infof("one")
+	buf.Infof("two")
+	if buf.Len() != 3 {
+		t.Fatalf("buf.Len() should be %d but was %d", 3, buf.Len())
+	}
+
+	if buf.At(0).Message != "zero" {
+		t.Fatalf("expected buf[0] %s but was %s", "zero", buf.At(0).Message)
+	}
+
+	buf.Infof("three")
+
+	if buf.At(0).Message != "one" {
+		t.Fatalf("expected buf[0] %s but was %s", "one", buf.At(0).Message)
+	}
+
+	buf.Infof("four")
+
+	if buf.At(0).Message != "two" {
+		t.Fatalf("expected buf[0] %s but was %s", "two", buf.At(0).Message)
+	}
+
+	buf.Infof("five")
+
+	if buf.At(0).Message != "three" {
+		t.Fatalf("expected buf[0] %s but was %s", "three", buf.At(0).Message)
+	}
+	if buf.At(1).Message != "four" {
+		t.Fatalf("expected buf[0] %s but was %s", "four", buf.At(1).Message)
+	}
+	if buf.At(2).Message != "five" {
+		t.Fatalf("expected buf[0] %s but was %s", "five", buf.At(2).Message)
+	}
+	if buf.At(3).Message != "three" {
+		t.Fatalf("expected buf[0] %s but was %s", "three", buf.At(3).Message)
+	}
+}
